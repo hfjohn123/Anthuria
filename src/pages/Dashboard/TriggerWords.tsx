b7a8fb5dbe34d6b568b9d4421daa96bbd1a2c04d
Qi <hfjohn123@live.com>
2024-06-19 17:13:28 -0400

    Updating Sort

--- src/pages/Dashboard/TriggerWords.tsx
+++ src/pages/Dashboard/TriggerWords.tsx
@@ -1,8 +1,8 @@
 import DefaultLayout from '../../layout/DefaultLayout';
 import axios from 'axios';
-import {useQuery} from '@tanstack/react-query';
-import {ChevronDownIcon, ChevronRightIcon, MagnifyingGlassIcon,} from '@heroicons/react/24/solid';
-import {useContext, useMemo, useState} from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { ChevronDownIcon, ChevronRightIcon, MagnifyingGlassIcon } from '@heroicons/react/24/solid';
+import { useContext, useMemo, useState } from 'react';
 import Loader from '../../common/Loader';
 import SelectGroupOne from '../../components/Forms/SelectGroup/SelectGroupOne.tsx';
 import {
@@ -17,9 +17,10 @@ import {
   GroupingState,
   Row,
   useReactTable,
+  SortingState
 } from '@tanstack/react-table';
-import {AuthContext} from '../../components/AuthWrapper.tsx';
-import NumberCards from "../../components/Cards/NumberCards.tsx";
+import { AuthContext } from '../../components/AuthWrapper.tsx';
+// import NumberCards from "../../components/Cards/NumberCards.tsx";
 
 type TriggerFinal = {
   facility_id: string;
@@ -35,24 +36,24 @@ type TriggerFinal = {
   created_by: string;
 };
 
-const renderSubComponent = ({row}: { row: Row<TriggerFinal> }) => {
+const renderSubComponent = ({ row }: { row: Row<TriggerFinal> }) => {
   return (
-    <div className = "flex bg-slate-100 dark:bg-slate-900 px-4 justify-evenly py-4">
+    <div className="flex bg-slate-100 dark:bg-slate-900 px-4 justify-evenly py-4">
       {/*<div>{row.getValue('revision_date')}</div>*/}
-      <div className = "basis-2/5">
-        <div className = "font-bold"> Progress Note: </div>
+      <div className="basis-2/5">
+        <div className="font-bold"> Progress Note:</div>
         {row.getValue('progress_note')}
-        <div className = "font-bold mt-2.5"> Progress Note ID: </div>
+        <div className="font-bold mt-2.5"> Progress Note ID:</div>
         {row.getValue('progress_note_id')}
-        <div className = "font-bold mt-2.5"> Created By: </div>
+        <div className="font-bold mt-2.5"> Created By:</div>
         {row.getValue('created_by')}
       </div>
-      <div className = "basis-2/5">
-        <div className = "font-bold">Summary: </div>
+      <div className="basis-2/5">
+        <div className="font-bold">Summary:</div>
         {row.getValue('summary')}
-        <div className = "font-bold mt-2.5">Trigger: </div>
+        <div className="font-bold mt-2.5">Trigger:</div>
         Fall
-        <div className = "font-bold mt-2.5">Generated Date: </div>
+        <div className="font-bold mt-2.5">Generated Date: </div>
         {row.getValue('report_date')}
       </div>
     </div>
@@ -60,10 +61,13 @@ const renderSubComponent = ({row}: { row: Row<TriggerFinal> }) => {
 };
 export default function TriggerWords() {
 
-  const {route} = useContext(AuthContext);
-  const {isPending, isError, data, error} = useQuery({
+  const { route } = useContext(AuthContext);
+  const [sorting, setSorting] = useState<SortingState>([]);
+
+
+  const { isPending, isError, data, error } = useQuery({
     queryKey: ['trigger-words', route],
-    queryFn: () => axios.get(`${route}/trigger_final`).then((res) => res.data),
+    queryFn: () => axios.get(`${route}/trigger_final`).then((res) => res.data)
   });
   const columns = useMemo<ColumnDef<TriggerFinal>[]>(
     () => [
@@ -71,8 +75,8 @@ export default function TriggerWords() {
         accessorKey: 'facility_id',
         header: 'Facility',
         meta: {
-          size: '100px',
-        },
+          size: '100px'
+        }
       },
 
       {
@@ -80,35 +84,35 @@ export default function TriggerWords() {
         header: 'Patient ID',
         filterFn: 'includesString',
         meta: {
-          size: '200px',
-        },
+          size: '200px'
+        }
       },
       {
         accessorKey: 'progress_note',
-        header: 'Progress Note',
+        header: 'Progress Note'
       },
       {
         accessorKey: 'progress_note_id',
         header: 'Progress Note ID',
         meta: {
-          size: '200px',
-        },
+          size: '200px'
+        }
       },
       {
         accessorKey: 'revision_date',
-        header: 'Revision Date',
+        header: 'Revision Date'
       },
       {
         accessorKey: 'summary',
-        header: 'Summary',
+        header: 'Summary'
       },
       {
         accessorKey: 'trigger_id',
-        header: 'Trigger ID',
+        header: 'Trigger ID'
       },
       {
         accessorKey: 'report_date',
-        header: 'Report Date',
+        header: 'Report Date'
       },
       {
         accessorKey: 'created_date',
@@ -116,21 +120,21 @@ export default function TriggerWords() {
         cell: (info) => {
           const date = new Date(info.getValue() as string | number | Date);
           return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
-        },
+        }
       },
       {
         accessorKey: 'status',
         header: 'Status',
         meta: {
-          size: '200px',
-        },
+          size: '200px'
+        }
       },
       {
         accessorKey: 'created_by',
-        header: 'Created By',
-      },
+        header: 'Created By'
+      }
     ],
-    [],
+    []
   );
   const [grouping, setGrouping] = useState<GroupingState>(['facility_id']);
   const [columnVisibility] = useState({
@@ -140,7 +144,7 @@ export default function TriggerWords() {
     trigger_id: false,
     created_by: false,
     report_date: false,
-    revision_date: false,
+    revision_date: false
   });
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
   const table = useReactTable({
@@ -150,156 +154,161 @@ export default function TriggerWords() {
       grouping,
       columnVisibility,
       columnFilters,
+      sorting
     },
+    onSortingChange: setSorting,
     onGroupingChange: setGrouping,
     getRowCanExpand: () => true,
     onColumnFiltersChange: setColumnFilters,
     getCoreRowModel: getCoreRowModel(),
-    getSortedRowModel: getSortedRowModel(),
     getExpandedRowModel: getExpandedRowModel(),
     getGroupedRowModel: getGroupedRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
+    getSortedRowModel: getSortedRowModel()
+
   });
   if (isPending) {
-    return <Loader/>;
+    return <Loader />;
   }
   if (isError) {
     return <div>Error: {error.message}</div>;
   }
   return (
-    <DefaultLayout title = {'Clinical Pulse'}>
-      <div className = "grid grid-cols-12">
+    <DefaultLayout title={'Clinical Pulse'}>
+      <div className="grid grid-cols-12">
         <SelectGroupOne
-          className = "col-span-9"
-          options = {['Fall']}
-          label = "Trigger Word / "
-          labelLeft = {true}
+          className="col-span-9"
+          options={['Fall']}
+          label="Trigger Word / "
+          labelLeft={true}
         />
-        <div className = "col-span-3">
+        <div className="col-span-3">
           <MagnifyingGlassIcon
-            className = "size-5 text-body dark:text-bodydark absolute translate-y-1/3 translate-x-1.5"/>
+            className="size-5 text-body dark:text-bodydark absolute translate-y-1/3 translate-x-1.5" />
           <input
-            onChange = {(e) => {
-              setColumnFilters([{id: 'patient_id', value: e.target.value}]);
+            onChange={(e) => {
+              setColumnFilters([{ id: 'patient_id', value: e.target.value }]);
             }}
-            placeholder = "Search Patient's name or ID"
-            className = " w-full py-1 rounded-lg border border-stroke pl-7 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+            placeholder="Search Patient's name or ID"
+            className=" w-full py-1 rounded-lg border border-stroke pl-7 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
           />
         </div>
         {/*<NumberCards className = "col-span-2 h-50 mt-5"/>*/}
-        <table className = "border-collapse mt-5 col-span-12 bg-white dark:bg-boxdark shadow-default ">
+        <table className="border-collapse mt-5 col-span-12 bg-white dark:bg-boxdark shadow-default ">
           <thead>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <tr key = {headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <th
-                      key = {header.id}
-                      colSpan = {header.colSpan}
-                      className = "py-3 pl-3 border-b-2 border-stroke dark:border-strokedark text-left "
-                      style = {{
-                        width: header.column.columnDef.meta?.size || 'auto',
-                      }}
-                    >
-                      {header.isPlaceholder ? null : (
-                        <span>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <th
+                    key={header.id}
+                    colSpan={header.colSpan}
+                    className="py-3 pl-3 border-b-2 border-stroke dark:border-strokedark text-left "
+                    onClick={header.column.getToggleSortingHandler()}
+                    role= "button"
+                    style={{
+                      width: header.column.columnDef.meta?.size || 'auto'
+                    }}
+                  >
+                    {header.isPlaceholder ? null : (
+                      <span>
                           {flexRender(
                             header.column.columnDef.header,
-                            header.getContext(),
+                            header.getContext()
                           )}
                         </span>
-                      )}
-                    </th>
-                  );
-                })}
-              </tr>
-            ))}
+                    )}
+                  </th>
+                );
+              })}
+            </tr>
+          ))}
           </thead>
           <tbody>
-            {table.getRowModel().rows.map((row) => {
-              return (
-                <>
-                  <tr key = {row.id} className = "border-t-stroke border-t">
-                    {row.getVisibleCells().map((cell) => {
-                      if (cell.getIsGrouped()) {
-                        return (
-                          <td
-                            key = {cell.id}
-                            className = "py-2"
-                            role = "button"
-                            onClick = {row.getToggleExpandedHandler()}
-                          >
-                            {row.getIsExpanded() ? (
-                              <ChevronDownIcon className = "text-body dark:text-bodydark size-6 inline pb-1"/>
-                            ) : (
-                              <ChevronRightIcon className = "text-body dark:text-bodydark size-6 inline pb-1"/>
-                            )}{' '}
-                            {flexRender(
-                              cell.column.columnDef.cell,
-                              cell.getContext(),
-                            )}
-                          </td>
-                        );
-                      }
-                      if (cell.getIsAggregated()) {
-                        return (
-                          <td
-                            key = {cell.id}
-                            className = "py-2"
-                            onClick = {row.getToggleExpandedHandler()}
-                            role = "button"
-                          ></td>
-                        );
-                      }
-                      if (cell.getIsPlaceholder()) {
-                        return <td key = {cell.id} className = "py-2"></td>;
-                      }
-                      if (cell.column.id === 'status') {
-                        return (
-                          <td key = {cell.id} className = "py-2">
-                            <button className = "text-red-500 border p-1 ">
-                              {flexRender(
-                                cell.column.columnDef.cell,
-                                cell.getContext(),
-                              )}
-                            </button>
-                          </td>
-                        );
-                      }
+          {table.getRowModel().rows.map((row) => {
+            return (
+              <>
+                <tr key={row.id} className="border-t-stroke border-t">
+                  {row.getVisibleCells().map((cell) => {
+                    if (cell.getIsGrouped()) {
                       return (
                         <td
-                          key = {cell.id}
-                          className = "py-2"
-                          role = "button"
-                          onClick = {row.getToggleExpandedHandler()}
+                          key={cell.id}
+                          className="py-2"
+                          role="button"
+                          onClick={row.getToggleExpandedHandler()}
                         >
-                          {cell.column.id === 'patient_id' ? (
-                            row.getIsExpanded() ? (
-                              <ChevronDownIcon className = "text-body dark:text-bodydark size-6 inline pb-1"/>
-                            ) : (
-                              <ChevronRightIcon className = "text-body dark:text-bodydark size-6 inline pb-1"/>
-                            )
+                          {row.getIsExpanded() ? (
+                            <ChevronDownIcon className="text-body dark:text-bodydark size-6 inline pb-1" />
                           ) : (
-                            ''
+                            <ChevronRightIcon className="text-body dark:text-bodydark size-6 inline pb-1" />
                           )}{' '}
                           {flexRender(
                             cell.column.columnDef.cell,
-                            cell.getContext(),
+                            cell.getContext()
                           )}
                         </td>
                       );
-                    })}
-                  </tr>
-                  {row.getIsExpanded() && row.depth === 1 && (
-                    <tr>
-                      <td colSpan = {row.getVisibleCells().length}>
-                        {renderSubComponent({row})}
+                    }
+                    if (cell.getIsAggregated()) {
+                      return (
+                        <td
+                          key={cell.id}
+                          className="py-2"
+                          onClick={row.getToggleExpandedHandler()}
+                          role="button"
+                        ></td>
+                      );
+                    }
+                    if (cell.getIsPlaceholder()) {
+                      return <td key={cell.id} className="py-2"></td>;
+                    }
+                    if (cell.column.id === 'status') {
+                      return (
+                        <td key={cell.id} className="py-2">
+                          <button className="text-red-500 border p-1 ">
+                            {flexRender(
+                              cell.column.columnDef.cell,
+                              cell.getContext()
+                            )}
+                          </button>
+                        </td>
+                      );
+                    }
+                    return (
+                      <td
+                        key={cell.id}
+                        className="py-2"
+                        role="button"
+                        onClick={row.getToggleExpandedHandler()}
+                      >
+                        {cell.column.id === 'patient_id' ? (
+                          row.getIsExpanded() ? (
+                            <ChevronDownIcon className="text-body dark:text-bodydark size-6 inline pb-1" />
+                          ) : (
+                            <ChevronRightIcon className="text-body dark:text-bodydark size-6 inline pb-1" />
+                          )
+                        ) : (
+                          ''
+                        )}{' '}
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
                       </td>
-                    </tr>
-                  )}
-                </>
-              );
-            })}
+                    );
+                  })}
+                </tr>
+                {row.getIsExpanded() && row.depth === 1 && (
+                  <tr>
+                    <td colSpan={row.getVisibleCells().length}>
+                      {renderSubComponent({ row })}
+                    </td>
+                  </tr>
+                )}
+              </>
+            );
+          })}
           </tbody>
         </table>
       </div>
