55bd82853ca9e7fcd03500b0b18577a0749008e0
hfjoh <hfjohn123@live.com>
2024-09-09 14:18:30 -0400

    Change to Tanstack Router Optimize a lot Fix the bug of due date not showing correctly.

--- src/common/dateRangeFilterFn.ts
+++ src/common/dateRangeFilterFn.ts
@@ -4,12 +4,7 @@ const dateRangeFilterFn = (
   filterValue: [Date, Date],
 ) => {
   const value = new Date(row.getValue(columnId) as string | number | Date);
-  return (
-    filterValue[0] <= value &&
-    new Date(
-      new Date(filterValue[1]).setDate(new Date(filterValue[1]).getDate() + 1),
-    ) >= value
-  );
+  return filterValue[0] <= value && filterValue[1] >= value;
 };
 
 export default dateRangeFilterFn;
